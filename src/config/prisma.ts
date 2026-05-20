ba49a54dd9d2e1888d3b20168afa87abdee4f438
Sandra <sandraunique230@gmail.com>
2026-05-20 10:13:17 +0200

    Fix prisma client for Render

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,16 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
-const pool = new pg.Pool({
-  connectionString: process.env["DATABASE_URL"],
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export const connectDB = async () => {
   try {
